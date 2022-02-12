e93f336e9e384e5c9f5bad09a6f80813e4511c7a
8BitRobot <premgiridhar11@gmail.com>
2022-02-11 21:20:36 -0800

    Restructured repository, cleaned up some unnecessary files, and added fonts.

--- frontend/src/Homepage/index.js
+++ frontend/src/Homepage/index.js
@@ -1,26 +1,15 @@
-import logo from './logo.svg';
+import logo from '../assets/logo.svg';
 import './Homepage.css';
 import { useEffect } from 'react';
 
 let client_id = "66e07c977ca0164c8fa6";
 let scopes = "read:user read:org repo"
-// let randomState;
 
 function Homepage() {
-  // let randomStateParams = "num=1&len=20&digits=on&upperalpha=on&loweralpha=on&unique=off&format=plain&rnd=new";
-  // fetch(
-  //   `https://www.random.org/strings/?${randomStateParams}`, {
-  // }).then(async (resp) => {
-  //   randomState = await randomState.text();
-  // });
   useEffect(async () => {
     const url = window.location.href;
     const urlParams = new URLSearchParams((new URL(url)).search);
     let code = urlParams.get("code");
-    // if (urlParams.get("state")) {
-    //   console.log("Uh-oh! Something bad is happening!");
-    // } else {
-    // }
 
     if (url.includes("?code=")) {
       let response = await (await fetch("http://localhost:4000/login", {

2c67ff5b4e0432a879acdc0d6bf934dfc8c48689
alex du <alexdu25@g.ucla.edu>
2022-02-28 14:13:32 -0800

    api-endpoint search

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 const app = express();
 const cors = require("cors");
 
@@ -8,15 +8,17 @@ require("dotenv").config({ path: "./config.env" });
 const axios = require("axios");
 
 const port = process.env.PORT || 4000;
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json());
 
-var db = require('./mongo.js');
+var db = require("./mongo.js");
 
 let { clientID, clientSecret } = require("./githubsso.json");
 const { MinKey } = require("mongodb");
@@ -25,51 +27,56 @@ app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
 
-app.get('/testAdd', function(req, res){
-    db.collection("test2").insertOne({name: "Rishab", last: "Khurana"}, function(err, res) {
-    if (err) console.log(err);
-    else console.log("inserted");
-  });
+app.get("/testAdd", function (req, res) {
+  db.collection("test2").insertOne(
+    { name: "Rishab", last: "Khurana" },
+    function (err, res) {
+      if (err) console.log(err);
+      else console.log("inserted");
+    }
+  );
   res.send("you tried");
 });
 
 function addToDb(item, col) {
-    var tempdb = db.collection(col);
-    tempdb.insertOne(item, function(err, res){
-	if (err) console.log(err);
-    });
+  var tempdb = db.collection(col);
+  tempdb.insertOne(item, function (err, res) {
+    if (err) console.log(err);
+  });
 }
 
 function getFromDb(item, col, pres) {
-    db.collection(col).find(item).toArray(function(err, res) {
-	pres.send(res);
+  db.collection(col)
+    .find(item)
+    .toArray(function (err, res) {
+      pres.send(res);
     });
 }
 
-app.post('/addComment', function(req, res) {
-    addToDb(req.body, "comment");
-    res.send(req.body);
+app.post("/addComment", function (req, res) {
+  addToDb(req.body, "comment");
+  res.send(req.body);
 });
 
-app.get('/getComment', function(req, res) {
-    getFromDb(req.body, "comment", res);
+app.get("/getComment", function (req, res) {
+  getFromDb(req.body, "comment", res);
 });
 
-app.get('/testGet', function(req, res) {
+app.get("/testGet", function (req, res) {
   console.log(req);
-  db.findOne({name: req.query.name}, function(err, dbres) {
+  db.findOne({ name: req.query.name }, function (err, dbres) {
     res.send(dbres);
   });
 });
 
-app.post('/searchDB', function(req, res) {
+app.post("/searchDB", function (req, res) {
   console.log(req.body);
-  db.find({}).toArray(function(err, dbres) {
+  db.find({}).toArray(function (err, dbres) {
     res.send(dbres);
   });
 });
 
-app.post('/login', function(req, res){
+app.post("/login", function (req, res) {
   if (req.cookies.token) {
     console.log(req.cookies.token);
     console.log("Token already exists!");
@@ -90,28 +97,38 @@ app.post('/login', function(req, res){
     res.send({
       status: 200,
     });
-  })
+  });
 });
 
 //return n most related
-function top(search, listDescript,n){
+function top(search, listDescript, n) {
   //n is 2
   //listDescript is list of descriptions[["java","object","oriented","slow"],["c++","fast"],["rust","new"]]
   //search is Set( ["fast","object"])( should be set)
-  let list =[];
-  for(let i=0;i<listDescript.length;i++){
-    let matches=0;
-    for(let element of listDescript[i]){
-      if(search.has(element)){
+  let list = [];
+  for (let i = 0; i < listDescript.length; i++) {
+    let matches = 0;
+    for (let element of listDescript[i]) {
+      if (search.has(element)) {
         matches++;
       }
     }
-    list.append([matches/listDescript[i].length,i]);
+    list.append([matches / listDescript[i].length, i]);
   }
-  list .sort(function(a,b){return a[0].compare(b[0])});
-  let ans=[];
-  for(let i =0;i<Math.min(n,list.length);i++){
+  list.sort(function (a, b) {
+    return a[0].compare(b[0]);
+  });
+  let ans = [];
+  for (let i = 0; i < Math.min(n, list.length); i++) {
     ans.append(list[i][1]);
   }
   return ans;
 }
+
+//endpoint?
+app.get("/searchReview", function (req, res) {});
+
+app.post("/searchReview", function (req, res) {
+  addToDb(req.body, "comment");
+  res.send(req.body);
+});
